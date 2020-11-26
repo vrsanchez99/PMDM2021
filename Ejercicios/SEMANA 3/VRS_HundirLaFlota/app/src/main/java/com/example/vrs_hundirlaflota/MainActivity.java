0012af1e807811b8ea344559f1f46c2b154d9fde
vrs99 <victoruizsanchez99@gmail.com>
2020-11-26 22:05:04 +0100

    hundir la flota (casi) terminado

--- Ejercicios/SEMANA 3/VRS_HundirLaFlota/app/src/main/java/com/example/vrs_hundirlaflota/MainActivity.java
+++ Ejercicios/SEMANA 3/VRS_HundirLaFlota/app/src/main/java/com/example/vrs_hundirlaflota/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.vrs_hundirlaflota;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.graphics.Color;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.TypedValue;
 import android.view.View;
@@ -12,38 +15,69 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private GridLayout tableroEnemigo;
     private GridLayout tableroJugador;
 
-    private MediaPlayer mediaPlayer;
+    private char[][] tableroEnemigo2;
+    private char[][] tableroJugador2;
+
+    private MediaPlayer mediaPlayer, sonidoError, sonidoAcierto;
+    private AudioManager audioManager;
+
+
+
     private char fallo = 'A';
     private char acierto = 'X';
+    private static char blank = '.';
 
-    private int posicionColumna =0, posicionFila=0;
+    private int posicionColumna = 0, posicionFila = 0;
+    private static final int BOARD_SIZE = 8;
+
 
     private TextView ptosBot, ptosJug;
     private Button btnCol, btnFila;
 
-    private int puntosBot = 14, puntosJugador=14;
-    private String[] columnas= new String[]{"A", "B", "C", "D", "E", "F", "G", "H"};
-    private String[]  filas = new String[]{"1", "2", "3", "4", "5" ,"6" , "7", "8"};
-    private boolean fin=false;
+    private int puntosBot = 14, puntosJugador = 14;
+    private String[] columnas = new String[]{"A", "B", "C", "D", "E", "F", "G", "H"};
+    private String[] filas = new String[]{"1", "2", "3", "4", "5", "6", "7", "8"};
+    private boolean fin = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mediaPlayer = new MediaPlayer();
+        mediaPlayer = MediaPlayer.create(this, R.raw.background_music);
+        mediaPlayer.start();
+        mediaPlayer.setLooping(true);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        }
+
+        int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        float percenth = 0.5f;
+        int half = (int) (maxVolume * percenth);
+        audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, half, 0);
+
+        sonidoAcierto = MediaPlayer.create(this, R.raw.acierto);
+        sonidoError = MediaPlayer.create(this, R.raw.fallo);
+
 
         btnCol = (Button) findViewById(R.id.btnColumna);
         btnFila = (Button) findViewById(R.id.btnFila);
 
         btnCol.setOnClickListener(this);
         btnFila.setOnClickListener(this);
+
+        tableroEnemigo2 = new char[BOARD_SIZE][BOARD_SIZE];
+        tableroJugador2 = new char[BOARD_SIZE][BOARD_SIZE];
 
         tableroEnemigo = new GridLayout(this);
         tableroJugador = new GridLayout(this);
@@ -52,34 +86,39 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         tableroJugador = (GridLayout) findViewById(R.id.tableroJugador);
 
 
-        ptosBot = (TextView)findViewById(R.id.txtPuntosBot);
-        ptosJug = (TextView)findViewById(R.id.txtPuntosJug);
-        ptosJug.setText(""+puntosJugador);
-        ptosBot.setText(""+puntosBot);
+        ptosBot = (TextView) findViewById(R.id.txtPuntosBot);
+        ptosJug = (TextView) findViewById(R.id.txtPuntosJug);
+        ptosJug.setText("" + puntosJugador);
+        ptosBot.setText("" + puntosBot);
 
 
         //Se rellenan ambos tableros con puntos, y luego de forma aleatoria
         //se colocan los barcos de cada uno.
-        llenarTablero(tableroEnemigo);
-        llenarTablero(tableroJugador);
+
+        colocarBarcos(tableroEnemigo2);
+        colocarBarcos(tableroJugador2);
+
+        llenarTablero(tableroEnemigo, tableroEnemigo2);
+        llenarTablero(tableroJugador, tableroJugador2);
 
     }
 
 
     /**
-     *
      * @param gridLayout
      */
-    public void llenarTablero(GridLayout gridLayout) {
+    public void llenarTablero(GridLayout gridLayout, char[][] tablero) {
 
         gridLayout.setColumnCount(8);
         gridLayout.setRowCount(8);
 
-        for (int i = 0; i < gridLayout.getRowCount(); i++) {
+        for (int i = 0; i < BOARD_SIZE; i++) {
 
-            for (int j = 0; j < gridLayout.getColumnCount(); j++) {
+            for (int j = 0; j < BOARD_SIZE; j++) {
 
-                gridLayout.addView(casilla());
+                TextView textView = casilla();
+                textView.setText(Character.toString(tablero[i][j]));
+                gridLayout.addView(textView);
 
             }
 
@@ -91,7 +130,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
     /**
-     *
      * @return
      */
     public TextView casilla() {
@@ -100,85 +138,182 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         textView.setLayoutParams(new ViewGroup.LayoutParams(
                 50,
                 50));
-
+        textView.setPadding(5, 5, 5, 5);
         textView.setTextSize(TypedValue.COMPLEX_UNIT_PX,
                 getResources().getDimension(R.dimen.result_font));
-        textView.setText(".");
         textView.setTextColor(Color.BLACK);
         textView.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
 
         return textView;
     }
 
+    private static int aleatorio() {
+
+        Random r = new Random(System.currentTimeMillis());
+        return r.nextInt(BOARD_SIZE);
+    }
+
     /**
      * Se colocan barcos de 1, 3 y 5 casillas de forma aleatoria.
-     * Habrán 3 barcos de 1 posicion, 2 barcos de 3 posiciones y 1 barco de 5 posiciones
-     * para cada tablero, respectivamente.
-     * @param tablero
+     * * Habrán 3 barcos de 1 posicion, 2 barcos de 3 posiciones y 1 barco de 5 posiciones
+     * * para cada tablero, respectivamente.
+     *
+     * @param mapa
      */
-    public void colocarBarcos(GridLayout tablero){
+    public static void colocarBarcos(char[][] mapa) {
 
 
+        for (int i = 0; i < BOARD_SIZE; i++)
+
+            for (int j = 0; j < BOARD_SIZE; j++)
+
+                mapa[i][j] = blank;
+
+//
+        int[] barcos = {5, 5, 3, 3, 3, 1, 1, 1, 1, 1};
+        char[] direccion = {'V', 'H'};
+
+        for (int b : barcos) {
+
+            boolean colocado = false;
+
+            while (!colocado) {
+
+                int fila = aleatorio();
+                int columna = aleatorio();
+                char direcc = direccion[aleatorio() % 2];
+
+                if (direcc == 'V') {
+
+                    if (fila + b <= (BOARD_SIZE - 1)) {
+
+                        boolean otro = false;
+
+                        for (int i = fila; (i <= fila + b) && !otro; i++) {
+
+                            if (mapa[i][columna] != blank)
+
+                                otro = true;
+                        }
+
+                        if (!otro) {
+
+                            for (int i = fila; i < fila + b; i++) {
+
+                                mapa[i][columna] = Integer.toString(b).charAt(0);
+                            }
+
+                            colocado = true;
+                        }
+                    }
+
+                } else {
+
+                    if (columna + b <= (BOARD_SIZE - 1)) {
+
+                        boolean otro = false;
+
+                        for (int j = columna; (j <= columna + b) && !otro; j++) {
+
+                            if (mapa[fila][j] != blank)
+
+                                otro = true;
+                        }
+
+                        if (!otro) {
+
+                            for (int j = columna; j < columna + b; j++) {
+
+                                mapa[fila][j] = Integer.toString(b).charAt(0);
+                            }
+
+                            colocado = true;
+                        }
+                    }
+                }
+            }
+        }
 
 
-        for (int i = 0; i < tablero.getChildCount(); i=+8) {
-            View child = tablero.getChildAt(i);
-            if (child instanceof TextView) {
-                ((TextView) child).setText("G");
+        /////////////
+
+
+    }
+
+    public void movimientoBot() {
+
+        int posicion = (int) (Math.random() * (63 - 1 + 1) + 1);
+
+        View child = tableroEnemigo.getChildAt(posicion);
+        if (child instanceof TextView) {
+
+            if (((TextView) child).getText().equals('.')) {
+
+                Toast.makeText(getApplicationContext(),
+                        "AGUA¡",
+                        Toast.LENGTH_LONG)
+                        .show();
+                sonidoError.start();
+                ((TextView) child).setText(Character.toString(fallo));
+
+
+            } else {
+
+                Toast.makeText(getApplicationContext(),
+                        "Te han golpeado¡",
+                        Toast.LENGTH_LONG)
+                        .show();
+                sonidoAcierto.start();
+                ((TextView) child).setText(Character.toString(acierto));
+                puntosJugador--;
+                ptosJug.setText(String.valueOf(ptosJug));
+                fin = evaluarGanador();
+
             }
 
         }
 
-
-
-
-
-    }
-
-    public void movimientoBot(){
-
-        int posicion = (int) (Math.random() * (63 - 1 + 1) + 1);
-
-
+        sonidoError.release();
+        sonidoAcierto.release();
 
 
     }
 
 
-    public int seleccionColumna(){
+    public int seleccionFila() {
 
 
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Selecciona la columna del tablero enemigo");
         builder.setItems(columnas, (dialog, pos) -> {
-            switch (columnas[pos]){
+            switch (columnas[pos]) {
 
                 case "A":
-                    posicionColumna = 0;
+                    posicionFila = 0;
                     break;
                 case "B":
-                    posicionColumna = 8;
+                    posicionFila = 8;
                     break;
                 case "C":
-                    posicionColumna = 16;
+                    posicionFila = 16;
                     break;
                 case "D":
-                    posicionColumna = 24;
+                    posicionFila = 24;
                     break;
                 case "E":
-                    posicionColumna = 32;
+                    posicionFila = 32;
                     break;
                 case "F":
-                    posicionColumna = 40;
+                    posicionFila = 40;
                     break;
                 case "G":
-                    posicionColumna =  48;
+                    posicionFila = 48;
                     break;
                 case "H":
-                    posicionColumna = 56;
+                    posicionFila = 56;
                     break;
                 default:
-
+                    posicionFila = -1;
 
             }
         }).show();
@@ -186,39 +321,39 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         return posicionColumna;
     }
 
-    public int seleccionFila(){
+    public int seleccionColumna() {
 
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Selecciona la fila del tablero enemigo");
         builder.setItems(filas, (dialog, pos) -> {
-            switch (filas[pos]){
+            switch (filas[pos]) {
 
                 case "1":
-                    posicionFila = 0;
+                    posicionColumna = 0;
                     break;
                 case "2":
-                    posicionFila = 1;
+                    posicionColumna = 1;
                     break;
                 case "3":
-                    posicionFila = 2;
+                    posicionColumna = 2;
                     break;
                 case "4":
-                    posicionFila = 3;
+                    posicionColumna = 3;
                     break;
                 case "5":
-                    posicionFila = 4;
+                    posicionColumna = 4;
                     break;
                 case "6":
-                    posicionFila = 5;
+                    posicionColumna = 5;
                     break;
                 case "7":
-                    posicionFila = 6;
+                    posicionColumna = 6;
                     break;
                 case "8":
-                    posicionFila = 7;
+                    posicionColumna = 7;
                     break;
                 default:
-
+                    posicionColumna = -1;
 
             }
         }).show();
@@ -232,7 +367,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
 
-        switch (v.getId()){
+        switch (v.getId()) {
 
             case R.id.btnColumna:
                 seleccionColumna();
@@ -243,6 +378,106 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
 
 
+        }
+
+    }
+
+
+    public void partida() {
+
+        while (!fin) {
+
+            boolean turnoJugador = false;
+            int fila = -1, col = -1;
+
+            while (turnoJugador == true) {
+
+                fila = seleccionFila();
+                col = seleccionColumna();
+
+                if (fila > -1 && col > -1) {
+
+                    //Se analiza la posicion escogida
+                    View child = tableroEnemigo.getChildAt(fila + (col - 1));
+                    if (child instanceof TextView) {
+
+                        if (((TextView) child).getText().equals('.')) {
+
+                            Toast.makeText(getApplicationContext(),
+                                    "AGUA¡",
+                                    Toast.LENGTH_LONG)
+                                    .show();
+                            sonidoError.start();
+                            ((TextView) child).setText(Character.toString(fallo));
+                            turnoJugador = false;
+
+                        } else {
+
+                            Toast.makeText(getApplicationContext(),
+                                    "ACIERTO¡",
+                                    Toast.LENGTH_LONG)
+                                    .show();
+                            sonidoAcierto.start();
+                            ((TextView) child).setText(Character.toString(acierto));
+                            turnoJugador = false;
+                            puntosBot--;
+                            ptosBot.setText(String.valueOf(ptosBot));
+                            fin = evaluarGanador();
+
+                        }
+
+                    }
+
+
+                }
+
+            }
+
+            sonidoAcierto.release();
+            sonidoError.release();
+
+            movimientoBot();
+
+        }
+
+        try {
+            finPartida();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public boolean evaluarGanador() {
+        if (puntosJugador == 0 || puntosBot == 0) return true;
+        else return false;
+
+    }
+
+    public void finPartida() throws InterruptedException {
+
+        btnCol.setEnabled(false);
+        btnFila.setEnabled(false);
+
+        Toast.makeText(getApplicationContext(),
+                "Fin de partida¡",
+                Toast.LENGTH_LONG)
+                .show();
+
+        Thread.sleep(1000);
+
+        if (puntosJugador == 0) {
+
+            Toast.makeText(getApplicationContext(),
+                    "PERDISTE¡",
+                    Toast.LENGTH_LONG)
+                    .show();
+
+        } else if (puntosBot == 0) {
+            Toast.makeText(getApplicationContext(),
+                    "GANASTE¡",
+                    Toast.LENGTH_LONG)
+                    .show();
         }
 
     }
